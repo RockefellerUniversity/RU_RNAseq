11a5ad0ca925ff8ebe3a1035e59b473f4c8ed3f0
root <tc.infomatics@gmail.com>
2020-07-12 19:18:13 +0000

    Autobuild

---
+++ docs/presentations/r_code/RU_RNAseq_p1.R
@@ -0,0 +1,403 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+condaSalmon <- CondaSysReqs::install_CondaTools("salmon","salmon")
+pathToSalmon <- file.path(dirname(dirname(condaSalmon$pathToConda)),"envs",condaSalmon$environment,"bin","salmon")
+
+if(!file.exists("ENCFF332KDA.fastq.gz")){
+  download.file("https://www.encodeproject.org/files/ENCFF332KDA/@@download/ENCFF332KDA.fastq.gz","ENCFF332KDA.fastq.gz")
+}
+if(!file.exists("ENCFF070QMF.fastq.gz")){
+  download.file("https://www.encodeproject.org/files/ENCFF070QMF/@@download/ENCFF070QMF.fastq.gz","ENCFF070QMF.fastq.gz")
+}
+
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides != "yes"){
+  cat("# RNAseq (part 1)
+
+
+"    
+  )
+  
+}
+
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Working with raw RNAseq data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Working with raw RNAseq data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----shortreada,include=FALSE-------------------------------------------------
+library(ShortRead)
+library(ggplot2)
+
+
+## ----tregFQ0,cache=TRUE, echo=F-----------------------------------------------
+#
+# fqSample <- FastqSampler("~/Downloads/ENCFF332KDA.fastq.gz",n=10^6)
+# fastq <- yield(fqSample)
+# writeFastq(fastq,"~/Documents/Box Sync/RU/Teaching/RU_side/RU_RNAseq/rnaseq/inst/extdata/data/ENCFF332KDA_sampled.fastq.gz")
+
+
+fastq <- readFastq("data/ENCFF332KDA_sampled.fastq.gz")
+
+
+## ----tregFQ1,eval=F, echo=T---------------------------------------------------
+## library(ShortRead)
+## fq<-"https://www.encodeproject.org/files/ENCFF332KDA/@@download/ENCFF332KDA.fastq.gz"
+## fqSample <- FastqSampler("ENCFF332KDA.fastq.gz",n=10^6)
+## fastq <- yield(fqSample)
+## fastq
+
+
+## ----tregFQ2,cache=TRUE,dependson="tregFQ0",fig.height=3,fig.width=7----------
+readSequences <- sread(fastq)
+readSequences_AlpbyCycle <- alphabetByCycle(readSequences)
+readSequences_AlpbyCycle[1:4,1:4]
+
+
+## ----tregFQ3,cache=TRUE,dependson="tregFQ2",fig.height=3,fig.width=7----------
+library(ggplot2)
+AFreq <- readSequences_AlpbyCycle["A",]
+CFreq <- readSequences_AlpbyCycle["C",]
+GFreq <- readSequences_AlpbyCycle["G",]
+TFreq <- readSequences_AlpbyCycle["T",]
+toPlot <- data.frame(Count=c(AFreq,CFreq,GFreq,TFreq),
+                     Cycle=rep(1:50,4),
+                     Base=rep(c("A","C","G","T"),each=50))
+ggplot(toPlot,aes(y=Count,x=Cycle,colour=Base))+geom_line()+theme_bw()
+
+
+## ----tregFQ4,cache=TRUE,dependson="tregFQ0",fig.height=2,fig.width=7----------
+readQuality <- quality(fastq)
+readQualityScores <- alphabetScore(readQuality)
+
+toPlot <- data.frame(ReadQ=readQualityScores)
+ggplot(toPlot,aes(x=ReadQ))+geom_histogram()+theme_minimal()
+
+
+## ----tregFQ55,cache=TRUE,dependson="tregFQ0",fig.height=5,fig.width=7---------
+qualAsMatrix <- as(readQuality,"matrix")
+boxplot(qualAsMatrix[1:10000,], outline=F)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Aligning RNAseq data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Aligning RNAseq data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----fa1q, include=FALSE------------------------------------------------------
+library(BSgenome.Mmusculus.UCSC.mm10)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+
+
+## ----fa1, echo=TRUE-----------------------------------------------------------
+library(BSgenome.Mmusculus.UCSC.mm10)
+BSgenome.Mmusculus.UCSC.mm10
+
+
+## ----fa2,cache=FALSE,echo=TRUE------------------------------------------------
+mainChromosomes <- paste0("chr",c(1:19,"X","Y","M"))
+mainChrSeq <- lapply(mainChromosomes,
+                     function(x)BSgenome.Mmusculus.UCSC.mm10[[x]])
+names(mainChrSeq) <- mainChromosomes
+mainChrSeqSet <- DNAStringSet(mainChrSeq)
+mainChrSeqSet
+
+
+## -----------------------------------------------------------------------------
+#writeXStringSet(mainChrSeqSet,"~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa")
+
+
+## ----fa3, echo=TRUE,eval=FALSE------------------------------------------------
+## 
+## writeXStringSet(mainChrSeqSet,
+##                 "BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa")
+
+
+## -----------------------------------------------------------------------------
+ # library(Rsubread)
+ # buildindex("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/mm10_mainchrs","~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa", memory=8000)
+
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## library(Rsubread)
+## buildindex("mm10_mainchrs","BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa",
+##            memory=8000,
+##            indexSplit=TRUE)
+## 
+
+
+## -----------------------------------------------------------------------------
+# library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+# library(rtracklayer)
+# tx <- transcriptsBy(TxDb.Mmusculus.UCSC.mm10.knownGene, 'gene')
+# gene_ids <- names(tx)
+# exons <- exons(TxDb.Mmusculus.UCSC.mm10.knownGene, columns = c('gene_id', 'tx_id', 'tx_name', 'exon_id'))
+# mcols(exons)$type <- 'exon'
+# mcols(exons)$source <- 'UCSC.mm10.knownGene'
+# mcols(exons)$transcript_id <- mcols(exons)$tx_id
+# exons<-exons[(as.vector(exons$gene_id) %>% sapply(length))>0]
+# export.gff(exons,con="~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/TxDb.Mmusculus.UCSC.mm10.knownGene.gtf",format="gtf")
+# 
+# subjunc("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/mm10_mainchrs","~/Downloads/ENCFF332KDA.fastq.gz",
+#         output_format = "BAM",
+#         output_file = "~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/Treg_1.bam",
+#       useAnnotation = TRUE,
+#       annot.ext = "~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/TxDb.Mmusculus.UCSC.mm10.knownGene.gtf",
+#       isGTF=TRUE)
+
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+## myExons <- exons(TxDb.Mmusculus.UCSC.mm10.knownGene,columns=c("tx_id","gene_id"))
+## myExons <- myExons[lengths(myExons$gene_id) == 1]
+## myExons
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## dfExons <- as.data.frame(myExons)
+## SAF <- data.frame(GeneID=dfExons$gene_id,
+##                   Chr=dfExons$seqnames,
+##                   Start=dfExons$start,
+##                   End=dfExons$end,
+##                   Strand=dfExons$strand)
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## 
+## myMapped <- subjunc("mm10_mainchrs",
+##                     "ENCFF332KDA.fastq.gz",
+##                     output_format = "BAM",
+##                     output_file = "Treg_1.bam",
+##                     useAnnotation = TRUE,
+##                     annot.ext = SAF,
+##                     isGTF=FALSE,
+##                     nthreads = 4)
+## 
+
+
+## -----------------------------------------------------------------------------
+# library(Rsamtools)
+# sortBam("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/Treg_1.bam","~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/Sorted_Treg_1")
+# indexBam("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/Treg_1.bam","~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/Sorted_Treg_1.bam")
+
+
+
+## ----sortindex, echo=TRUE,eval=FALSE------------------------------------------
+## library(Rsamtools)
+## sortBam("Treg_1.bam","Sorted_Treg_1")
+## indexBam("Sorted_Treg_1.bam")
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Counting with aligned RNAseq data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Counting with aligned RNAseq data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----gm, echo=TRUE,eval=TRUE,cache=TRUE---------------------------------------
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+geneExons <- exonsBy(TxDb.Mmusculus.UCSC.mm10.knownGene,by="gene")
+class(geneExons)
+
+
+## ----gm2,echo=TRUE,eval=TRUE,cache=TRUE,dependson="gm"------------------------
+geneExons[1:2]
+
+
+## ----gm0,echo=TRUE,eval=F-----------------------------------------------------
+## library(GenomicAlignments)
+## myBam <- BamFile("Sorted_Treg_1.bam",yieldSize = 10000)
+## tregGeneCounts <- summarizeOverlaps(geneExons,myBam,
+##                                     ignore.strand = TRUE)
+## tregGeneCounts
+
+
+## ----gm3,echo=FALSE,eval=TRUE,cache=TRUE--------------------------------------
+load("data/myTregCounts2020.RData")
+tregGeneCounts
+
+
+## ----em2,echo=TRUE,eval=TRUE,cache=TRUE,dependson="em"------------------------
+library(GenomicFeatures)
+nonOverlappingExons <- disjointExons(TxDb.Mmusculus.UCSC.mm10.knownGene)
+names(nonOverlappingExons) <- paste(mcols(nonOverlappingExons)$gene_id,
+                                    mcols(nonOverlappingExons)$exonic_part,
+                                    sep="_")
+nonOverlappingExons[1:3,]
+
+
+## ----em3,echo=TRUE,eval=FALSE,cache=TRUE,dependson="em2"----------------------
+## tregExonCounts <- summarizeOverlaps(nonOverlappingExons,
+##                                     myBam,
+##                                     ignore.strand = TRUE,
+##                                     inter.feature=FALSE)
+
+
+## ----em4A,echo=FALSE,eval=TRUE,cache=TRUE-------------------------------------
+load("data/myTregExonCounts2020.RData")
+
+
+## ----em4,echo=TRUE,eval=TRUE,cache=TRUE,dependson=c("em4A","gm3")-------------
+geneCounts <- assay(tregGeneCounts)
+exonCounts <- assay(tregExonCounts)
+head(exonCounts,2)
+head(geneCounts,2)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Transcript quantification with pseudo-alignment
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Transcript quantification with pseudo-alignment
+
+
+"    
+  )
+  
+}
+
+
+
+## ----sal1,echo=TRUE,eval=TRUE,cache=TRUE--------------------------------------
+allTxSeq <- extractTranscriptSeqs(BSgenome.Mmusculus.UCSC.mm10,
+                      TxDb.Mmusculus.UCSC.mm10.knownGene,
+                      use.names=TRUE)
+allTxSeq
+
+
+## ----sal2,echo=F,eval=TRUE,cache=TRUE,dependson="sal1"------------------------
+writeXStringSet(allTxSeq,"mm10Trans.fa")
+
+
+## ----sal0,echo=TRUE,eval=F----------------------------------------------------
+## writeXStringSet(allTxSeq,
+##                 "mm10Trans.fa")
+
+
+## ----salI_1,echo=TRUE,eval=FALSE,dependson="sal1"-----------------------------
+## salmonExec <- paste0(pathToSalmon," index")
+## fastaTx <- "mm10Trans.fa"
+## indexName <- "mm10Trans"
+## salmonIndexCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-t",fastaTx)
+## salmonIndexCmd
+## system(salmonIndexCmd, wait = TRUE)
+
+
+## ----salI_1a,echo=FALSE,eval=TRUE,cache=TRUE,dependson="sal1", warning=F, message=F----
+# setwd("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/")
+# salmonExec <- "/Users/mattpaul/miniconda3/envs/rnaseq/bin/salmon index"
+# fastaTx <- "mm10Trans.fa"
+# indexName <- "mmm10Trans"
+# salmonIndexCmd <- paste(salmonExec,
+#                         "-i",indexName,
+#                         "-t",fastaTx)
+# system(salmonIndexCmd, wait = TRUE)
+
+
+
+## ----salQ_1,echo=TRUE,eval=FALSE----------------------------------------------
+## salmonExec <- paste0(pathToSalmon," quant")
+## fq <- "ENCFF332KDA.fastq.gz"
+## outDir <- "TReg_1_Quant"
+## salmonQuantCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-o",outDir,
+##                         "-l A",
+##                         "-r",fq)
+## salmonQuantCmd
+## system(salmonQuantCmd, wait = TRUE)
+
+
+## ----salQ_1a,echo=FALSE,eval=TRUE,cache=TRUE,dependson="sal1", warning=F, message=F----
+
+# setwd("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/")
+# salmonExec <- "/Users/mattpaul/miniconda3/envs/rnaseq/bin/salmon quant"
+# fq <- "ENCFF332KDA.fastq.gz"
+# outDir <- "TReg_1_Quant"
+# salmonQuantCmd <- paste(salmonExec,
+#                         "-l A",
+#                         "-i",indexName,
+#                         "-r",fq,
+#                         "-o",outDir)
+# system(salmonQuantCmd , wait = TRUE)
+
+
+## ----salReadIn,echo=TRUE,eval=FALSE,cache=TRUE--------------------------------
+## myQuant <- read.delim("TReg_1_Quant/quant.sf")
+## myQuant[1:3,]
+
+
+## ----salReadIn2,echo=FALSE,eval=TRUE,cache=TRUE-------------------------------
+myQuant <- read.delim("data/TReg_1_Quant/quant.sf")
+myQuant[1:3,]
+
---
+++ r_course/presentations/r_code/RU_RNAseq_p2.R
@@ -0,0 +1,404 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+
+condaSalmon <- CondaSysReqs::install_CondaTools("salmon","salmon")
+pathToSalmon <- file.path(dirname(dirname(condaSalmon$pathToConda)),"envs",condaSalmon$environment,"bin","salmon")
+
+
+## ----neQants,include=FALSE,eval=FALSE-----------------------------------------
+## 
+## salmonExec <- paste0(pathToSalmon," quant")
+## fq <- "ENCFF070QMF.fastq.gz"
+## outDir <- "TReg_2_Quant"
+## salmonQuantCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-o",outDir,
+##                         "-l A",
+##                         "-r",fq)
+## salmonQuantCmd
+## system(salmonQuantCmd, wait = TRUE)
+## 
+## salmonExec <- paste0(pathToSalmon," quant")
+## fq <- "ENCFF144YYI.fastq.gz"
+## outDir <- "TReg_act_1_Quant"
+## salmonQuantCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-o",outDir,
+##                         "-l A",
+##                         "-r",fq)
+## salmonQuantCmd
+## system(salmonQuantCmd, wait = TRUE)
+## 
+## salmonExec <- paste0(pathToSalmon," quant")
+## fq <- "ENCFF042XBW.fastq.gz"
+## outDir <- "TReg_act_2_Quant"
+## salmonQuantCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-o",outDir,
+##                         "-l A",
+##                         "-r",fq)
+## salmonQuantCmd
+## system(salmonQuantCmd, wait = TRUE)
+## 
+## salmonExec <- paste0(pathToSalmon," quant")
+## fq <- "ENCFF053CFZ.fastq.gz"
+## outDir <- "TReg_act_3_Quant"
+## salmonQuantCmd <- paste(salmonExec,
+##                         "-i",indexName,
+##                         "-o",outDir,
+##                         "-l A",
+##                         "-r",fq)
+## salmonQuantCmd
+## system(salmonQuantCmd, wait = TRUE)
+## 
+## 
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides != "yes"){
+  cat("# RNAseq (part 2)
+
+
+"    
+  )
+  
+}
+
+
+
+## ----setup, include=FALSE-----------------------------------------------------
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(GenomicAlignments)
+library(DESeq2)
+library(tximport)
+library(org.Mm.eg.db)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Counting with multiple RNAseq datasets
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Counting with multiple RNAseq datasets
+
+
+"    
+  )
+  
+}
+
+
+
+## ----eval=FALSE,echo=TRUE-----------------------------------------------------
+## library(Rsamtools)
+## bamFilesToCount <- c("Sorted_Treg_1.bam","Sorted_Treg_2.bam",
+##                      "Sorted_Treg_act_1.bam","Sorted_Treg_act_2.bam",
+##                      "Sorted_Treg_act_3.bam")
+## names(bamFilesToCount) <- c("Sorted_Treg_1","Sorted_Treg_2",
+##                      "Sorted_Treg_act_1","Sorted_Treg_act_2",
+##                      "Sorted_Treg_act_3")
+## myBams <- BamFileList(bamFilesToCount,yieldSize = 10000)
+## 
+
+
+## ----eval=FALSE,echo=TRUE-----------------------------------------------------
+## library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+## library(GenomicAlignments)
+## geneExons <- exonsBy(TxDb.Mmusculus.UCSC.mm10.knownGene,by="gene")
+## geneCounts <- summarizeOverlaps(geneExons,myBams,
+##                                     ignore.strand = TRUE)
+## geneCounts
+
+
+## ----gC1,eval=TRUE,echo=FALSE,cache=TRUE--------------------------------------
+load("data/GeneCounts.RData")
+geneCounts
+
+
+## ----bp,eval=FALSE,echo=TRUE,cache=TRUE---------------------------------------
+## library(BiocParallel)
+
+
+## ----bp1,eval=FALSE,echo=TRUE,cache=TRUE,dependson="bp"-----------------------
+## paramMulti <- MulticoreParam(workers=2)
+## paramSerial <- SerialParam()
+## register(paramSerial)
+
+
+## ----gC2,eval=TRUE,echo=TRUE,cache=TRUE,dependson="gC1"-----------------------
+assay(geneCounts)[1:2,]
+
+
+## ----gC3,eval=TRUE,echo=TRUE,cache=TRUE,dependson="gC1"-----------------------
+rowRanges(geneCounts)[1:2,]
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Differential gene expression analysis
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Differential gene expression analysis
+
+
+"    
+  )
+  
+}
+
+
+
+## ----de1,eval=TRUE,echo=TRUE,cache=TRUE,dependson="gC1"-----------------------
+metaData <- data.frame(Group=c("Naive","Naive","Act","Act","Act"),
+                       row.names = colnames(geneCounts))
+metaData
+
+
+## ----de22,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de1"----------------------
+countMatrix <- assay(geneCounts)
+countGRanges <- rowRanges(geneCounts)
+dds <- DESeqDataSetFromMatrix(countMatrix,
+                              colData = metaData,
+                              design = ~Group,
+                              rowRanges=countGRanges)
+dds
+
+
+## ----de2,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de1"-----------------------
+colData(geneCounts)$Group <- metaData$Group
+geneCounts
+
+
+## ----de3,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de1"-----------------------
+dds <- DESeqDataSet(geneCounts,design = ~Group)
+dds
+
+
+## ----de4,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de3"-----------------------
+dds <- DESeq(dds)
+
+
+## ----de5,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de4"-----------------------
+normCounts  <- counts(dds, normalized=TRUE)
+normCounts[1:2,]
+
+
+## ----de6,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de4",fig.width=8,fig.height=4.5----
+plotDispEsts(dds)
+
+
+## ----de7,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de4"-----------------------
+myRes <-results(dds,contrast = c("Group","Act","Naive"))
+myRes <- myRes[order(myRes$pvalue),]
+myRes[1:3,]
+
+
+## ----dr1,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7")--------------------
+summary(myRes)
+
+
+## ----drccd1a,eval=FALSE,echo=TRUE---------------------------------------------
+## plotMA(myRes)
+
+
+## ----dr1a,eval=TRUE,echo=FALSE,cache=TRUE,dependson=c("de7"),fig.height=4,fig.width=7----
+DESeq2::plotMA(myRes)
+
+
+## ----dr1b,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7"),fig.height=4,fig.width=7----
+myRes_lfc <- lfcShrink(dds, coef =  "Group_Naive_vs_Act")
+DESeq2::plotMA(myRes_lfc)
+
+
+## ----dr1c,eval=TRUE,echo=FALSE,cache=TRUE,dependson=c("dr1b")-----------------
+myRes <-results(dds,contrast = c("Group","Act","Naive"))
+
+
+## ----dr2,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7")--------------------
+myResAsDF <- as.data.frame(myRes)
+myResAsDF[1:2,]
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Significance and Multiple Testing
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Significance and Multiple Testing
+
+
+"    
+  )
+  
+}
+
+
+
+## ----dr22a,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7")------------------
+table(is.na(myResAsDF$padj))
+
+
+## ----dr22b,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7")------------------
+myResAsDF$newPadj <- p.adjust(myResAsDF$pvalue)
+myResAsDF[1:3,]
+
+
+## ----dr22,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("de7")-------------------
+myResAsDF <- myResAsDF[!is.na(myResAsDF$padj),]
+myResAsDF[1:3,]
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# DEseq2 and Salmon
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# DEseq2 and Salmon
+
+
+"    
+  )
+  
+}
+
+
+
+## ----tx1,eval=TRUE,echo=TRUE,cache=TRUE,dependson="de4"-----------------------
+library(tximport)
+
+
+## -----------------------------------------------------------------------------
+# setwd("/Users/mattpaul/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/RNAseq_course/")
+# salmonExec <- "/Users/mattpaul/miniconda3/envs/rnaseq/bin/salmon quant"
+# fq <- "ENCFF070QMF.fastq.gz"
+# outDir <- c("TReg_2_Quant")
+# indexName <- "../mmm10Trans"
+# salmonQuantCmd <- paste(salmonExec,
+#                          "-l A",
+#                          "-i",indexName,
+#                          "-r",fq,
+#                          "-o",outDir)
+# system(salmonQuantCmd , wait = TRUE)
+
+
+## ----tx2,eval=TRUE,echo=TRUE,cache=TRUE,dependson="tx1"-----------------------
+temp <- read.delim("data/Salmon/TReg_2_Quant/quant.sf")
+
+temp[1:3,]
+
+
+## ----tx3a,eval=TRUE,echo=TRUE,cache=TRUE,dependson="tx2",warning=FALSE,message=FALSE----
+
+Tx2Gene <- select(TxDb.Mmusculus.UCSC.mm10.knownGene,
+                  keys = as.vector(temp[,1]),
+                  keytype = "TXNAME",
+                  columns = c("GENEID","TXNAME"))
+Tx2Gene <- Tx2Gene[!is.na(Tx2Gene$GENEID),]
+
+
+## ----tx4,eval=TRUE,echo=TRUE,cache=TRUE,dependson="tx3a"----------------------
+salmonQ <- dir("data/Salmon/",recursive = T,
+               pattern = "quant.sf",full.names = T)
+salmonCounts <- tximport(salmonQ,
+                         type="salmon",
+                         tx2gene = Tx2Gene)
+
+
+## ----tx5,eval=TRUE,echo=TRUE,cache=TRUE,dependson="tx4"-----------------------
+salmonCounts$abundance[1:2,]
+salmonCounts$counts[1:2,]
+
+
+## ----tx6,eval=TRUE,echo=TRUE,cache=TRUE,dependson="tx5"-----------------------
+ddsSalmon <- DESeqDataSetFromTximport(salmonCounts,
+                                      colData = metaData,
+                                      design = ~Group)
+
+
+## ----tx7,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("tx6"),message=FALSE,warning=FALSE----
+ddsSalmon <- DESeq(ddsSalmon)
+myResS <-results(ddsSalmon,contrast = c("Group","Act","Naive"))
+myResS <- myResS[order(myResS$pvalue),]
+myResS[1:3,]
+
+
+## ----dr1ss,eval=TRUE,echo=FALSE,cache=TRUE,dependson=c("tx7","de7")-----------
+swde <- merge(as.data.frame(myRes),as.data.frame(myResS),by=0,all=FALSE)
+
+toCompare <- cbind(abs(swde$log2FoldChange.x) > 1 & swde$padj.x < 0.05 & !is.na(swde$padj.x) & !is.na(swde$padj.y), abs(swde$log2FoldChange.y) > 1 & swde$padj.y < 0.05 & !is.na(swde$padj.y) & !is.na(swde$padj.x))
+colnames(toCompare) <- c("summarizeOverlaps","Salmon")
+limma::vennDiagram(toCompare)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Adding Annotation
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Adding Annotation
+
+
+"    
+  )
+  
+}
+
+
+
+## ----anno,eval=TRUE,echo=TRUE,cache=TRUE,dependson=c("tx7","de7"),message=FALSE,warning=FALSE----
+library(org.Mm.eg.db)
+eToSym <- select(org.Mm.eg.db,
+                 keys = rownames(myResAsDF),
+                 keytype = "ENTREZID",
+                 columns="SYMBOL")
+eToSym[1:10,]
+
+
+## ----anno2,eval=TRUE,echo=TRUE,cache=TRUE,dependson="anno"--------------------
+annotatedRes <- merge(eToSym,myResAsDF,
+                      by.x=1,
+                      by.y=0,
+                      all.x=FALSE,
+                      all.y=TRUE)
+annotatedRes <- annotatedRes[order(annotatedRes$pvalue),]
+annotatedRes[1:3,]
+
